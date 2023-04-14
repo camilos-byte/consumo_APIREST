16fb6dbf17c4496335713355096647df61f66d5c
solicitudes.informatica <solicitudes.informatica@andar.com.co>
2023-04-14 15:49:29 -0500

    actualizacion 14

---
+++ gpt_4.js
@@ -0,0 +1,9 @@
+function res(num1 , num2) {
+    console.log(Math.floor( num1 / num2))
+    // console.log(num1 % num2)
+    // console.log( num2 * 3)
+}
+
+res (70,22)
+
+// Math.ceil ((numDays % 22) * dayRate (ratePerHour))
--- main.js
+++ main.js
@@ -8,32 +8,56 @@
           }) */
 console.log('CARGA COMPLETA');
 
-const URL_API_SEARCH_RANDOM = 'https://api.thedogapi.com/v1/images/search?limit=4&api_key=live_nYwwvicoRKmzbTpDh7sS1MhVASUDuCY8QTYPWlV7lUa5LDRjRk4ydl0p9kT4a7bl';
+const endpoint = {
+    URL_API_SEARCH_RANDOM : 'https://api.thedogapi.com/v1/images/search?limit=4&api_key=live_nYwwvicoRKmzbTpDh7sS1MhVASUDuCY8QTYPWlV7lUa5LDRjRk4ydl0p9kT4a7bl',
+    URL_API_FAVOURITES: 'https://api.thedogapi.com/v1/favourites?api_key=live_nYwwvicoRKmzbTpDh7sS1MhVASUDuCY8QTYPWlV7lUa5LDRjRk4ydl0p9kT4a7bl',
+}
+
+
 const btn_recarga = document.querySelector("#recargaImagen");
 const img = document.querySelector("#imagenesDog");
 async function fetchData() {
-    const resp = await fetch(URL_API_SEARCH_RANDOM);
+    const resp = await fetch(endpoint.URL_API_SEARCH_RANDOM);
     const data = await resp.json();
-    console.log(data);
-    let datos_imagen = `
+    try {
+        console.log(data);
+         let datos_imagen = `
     ${data.map (img =>
         ` 
         <div class="col-auto mb-3 me-2 ms-3" style="float: left;">
             <div class="card " style="width: 18rem;" >
                 <img src="${img.url}" class="card-img-top img_cambia" alt="..." width="300" height="300">
                 <div class="card-body">
-                    <button type="button" class="btn btn-primary">Agregar a Favoritos</button>
+                   <button type="button" id="${img.id}" class="btn btn-primary ">Agregar a Favoritos</button>
                 </div>
             </div>
         </div>
         `)}`;
         img.innerHTML = datos_imagen.replace(/,/g,"") 
+    } catch (error) {
+        const errorNode = document.querySelector('#error');
+        errorNode.innerText = `Error: ${error.message}`;
+    }
+   
     }
     // boton carga nuevas imagenes 
     btn_recarga.onclick = fetchData
     fetchData()
 
-    setTimeout(()=>{
-        let imagenDog = document.querySelectorAll(".img_cambia");
-         imagenDog.forEach((dog)=>{ dog.addEventListener("mouseover",()=>{console.log("Holaaaaaaaaaaaaaaaaa");})})
-    }, 2000)
+ 
+   
+
+
+//  async function saveFavoritesDogs (){
+//     const option = {
+//         method:'POST',
+//         headers: {
+//             'content-type': 'application/json'
+//         },
+//         body: JSON.stringify(
+//             {image_id: 'UyN27ZccR'}
+//         )
+//     }
+//     const res = await fetch (endpoint.URL_API_FAVOURITES, option)
+//     console.log("SAVE");
+//  }
