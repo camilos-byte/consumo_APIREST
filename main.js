92d49e3f7804b1487326712b00f544f08b910c6a
solicitudes.informatica <solicitudes.informatica@andar.com.co>
2023-05-11 12:52:52 -0500

    Ultimos cambios

--- main.js
+++ main.js
@@ -9,11 +9,12 @@
 console.log('CARGA COMPLETA');
 const d = document;
 const endpoint = {
-    URL_API_SEARCH_FAVOURITES : 'https://api.thedogapi.com/v1/favourites/api_key=live_nYwwvicoRKmzbTpDh7sS1MhVASUDuCY8QTYPWlV7lUa5LDRjRk4ydl0p9kT4a7bl' ,
-    URL_API_SEARCH_RANDOM : 'https://api.thedogapi.com/v1/images/search?limit=4&api_key=live_nYwwvicoRKmzbTpDh7sS1MhVASUDuCY8QTYPWlV7lUa5LDRjRk4ydl0p9kT4a7bl',
-    URL_API_FAVOURITES: 'https://api.thedogapi.com/v1/favourites?api_key=live_nYwwvicoRKmzbTpDh7sS1MhVASUDuCY8QTYPWlV7lUa5LDRjRk4ydl0p9kT4a7bl',
+    // URL_API_SEARCH_FAVOURITES : 'https://api.thedogapi.com/v1/favourites/api_key=live_how4GJ5F7BqESedRg4YV13F5gVCGELLDWCAhGW3j5ibdvuGkPETeRohdq517FuFQ' ,
+    URL_API_SEARCH_RANDOM : 'https://api.thedogapi.com/v1/images/search?limit=4&api_key=live_how4GJ5F7BqESedRg4YV13F5gVCGELLDWCAhGW3j5ibdvuGkPETeRohdq517FuFQ',
+    URL_API_FAVOURITES: 'https://api.thedogapi.com/v1/favourites?api_key=live_how4GJ5F7BqESedRg4YV13F5gVCGELLDWCAhGW3j5ibdvuGkPETeRohdq517FuFQ',
 }
 
+let URL_API_DELETE_FAV = (id) => `https://api.thedogapi.com/v1/favourites/${id}?api_key=live_how4GJ5F7BqESedRg4YV13F5gVCGELLDWCAhGW3j5ibdvuGkPETeRohdq517FuFQ`;
 
 const btn_recarga = document.querySelector("#recargaImagen");
 const img = document.querySelector("#imagenesDog");
@@ -26,9 +27,9 @@ async function fetchData() {
          let datos_imagen = `
     ${data.map (img =>
         ` 
-        <div class="col mb-3 me-2 ms-3" style="float: left;">
+        <div class="" >
             <div class="card " style="width: 18rem;" >
-                <img src="${img.url}" class="card-img-top img_cambia" alt="..." width="300" height="300">
+                <img src="${img.url}" class="card-img-top img_cambia" alt="..." width="300" height="200">
                 <div class="card-body">
                    <button type="button" id="${img.id}" class="btn btn-primary btnAdd">Agregar a Favoritos</button>
                 </div>
@@ -41,9 +42,11 @@ async function fetchData() {
          btnAdd.forEach((btnAddcats)=>{
            btnAddcats.addEventListener('click', (e)=>{
               let id = e.target.id
-               saveFavoritesDogs (id)
+               saveFavoritesDogs (id);
+               btnAddcats.setAttribute("disabled", "");
               })
             })
+
     } catch (error) {
         const errorNode = document.querySelector('#error');
         errorNode.innerText = `Error: ${error.message}`;
@@ -52,43 +55,86 @@ async function fetchData() {
     }
     
     async function saveFavoritesDogs (id){
+       
         // console.log(id);
         var option = { 
             method:'POST', 
             headers: { 'content-type': 'application/json' },
             body: JSON.stringify( {image_id: id})
         }
-        const res = await fetch (endpoint.URL_API_FAVOURITES, option)
-        console.log(res)
+        const res = await fetch (endpoint.URL_API_FAVOURITES, option);
         const data = await res.json();
-        console.log(data);
-        if (res.status == 200) {
-            Swal.fire({
-                icon: 'success',
-                title: 'Agregado a favoritos',
-                text: `El id de su perro favorito es ` + data.id
-            })
-            dogsFovurites (data.id)    
-        }
+        console.log(res);
+            if (res.status == 200) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Agregado a favoritos',
+                    text: `El id de su perro favorito es ` + data.id
+                })
+                
+                dogsFovurites (data.id);    
+            }
     }
 
 
     async function dogsFovurites (subId) {
+        let favDogs = d.querySelector('#favDogs');
         const res = await fetch(`${endpoint.URL_API_FAVOURITES}?sub_id=`+subId)
-        console.log(res);
         const data = await res.json();
-        console.log(data);
         try {
+            console.log(data);
             let data_nuevo = data.slice(data.length-4)
-            console.log(data_nuevo);
-            console.log(data_nuevo[3].image.url); 
-            
+            let datos_fav = `
+            ${data_nuevo.map (dog_fav =>
+                ` 
+                <div class="col mb-3 me-2 ms-3" style="float: left;">
+                    <div class="card " style="width: 18rem;" >
+                        <img src="${dog_fav.image.url}" class="card-img-top img_cambia" alt="..." width="300" height="200">
+                        <div class="card-body">
+                           <button type="button" id="${dog_fav.id}" class="btn btn-primary btn_delete">Eliminar</button>
+                        </div>
+                    </div>
+                </div>
+                `)}`;
+                favDogs.innerHTML = datos_fav.replace(/,/g,"") 
+                   
+        let btnDelete = d.querySelectorAll(".btn_delete");
+        btnDelete.forEach((btnDeletecats)=>{
+            btnDeletecats.addEventListener('click', (e)=>{
+             let id = e.target.id
+             console.log(id);
+             deleteFavorites(id)
+             })
+           })
         } catch (error) {
             const errorNode = document.querySelector('#error');
             errorNode.innerText = `Error: ${error.message}`;
         }
     }
     
+
+    async function deleteFavorites(id) {
+       const resDelete = await fetch (URL_API_DELETE_FAV(id),{
+        method: 'DELETE'
+       })
+       const dataDelete = await resDelete.json();
+       console.log(dataDelete);
+       if (resDelete.status == 200) {
+        Swal.fire({
+            icon: 'success',
+            title: 'Eliminado',
+            text: `Eliminado`
+        })
+
+        dogsFovurites(id);
+       } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oopss',
+            text: `No se ha podido eliminar` +id
+            })
+       }
+    }
     // boton carga nuevas imagenes 
     btn_recarga.onclick = fetchData
     fetchData()
